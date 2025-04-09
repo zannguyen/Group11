2a98fd0e018bb3f9838f36d0abadab35e06104ea
phucnguyenngoc <nguyenngocphuc7@dtu.edu.vn>
2025-04-09 23:24:43 +0700

    viet ham chia

--- BT1/src/view/CHia.java
+++ BT1/src/view/CHia.java
@@ -1,5 +1,8 @@
 package view;
 
 public class CHia {
-
+	 public static String tinh(int a, int b) {
+	        if (b == 0) return "Không chia được cho 0";
+	        return String.valueOf(a / b);
+	    }
 }
