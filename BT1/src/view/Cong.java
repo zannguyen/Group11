91cf1acb5d2c460706a9df3f28c85995b2138937
DUY ANH <email@domain.com>
2025-04-09 23:16:47 +0700

    Update

--- BT1/src/view/Cong.java
+++ BT1/src/view/Cong.java
@@ -2,10 +2,8 @@ package view;
 
 public class Cong {
     public static int tinh(int a, int b) {
-    	if(a > 0 || b> 0) {
-    		  return a + b;
-    	}
-    	return 0;
+     return a + b;
+
       
     }
 }
