647dceb3dff2ef946d7ff1c42265cd3231bbb4f9
phucnguyenngoc <nguyenngocphuc7@dtu.edu.vn>
2025-04-09 23:26:58 +0700

    fix bug

--- BT1/src/view/CHia.java
+++ BT1/src/view/CHia.java
@@ -1,8 +1,9 @@
 package view;
 
 public class CHia {
-	 public static String tinh(int a, int b) {
-	        if (b == 0) return "Không chia được cho 0";
-	        return String.valueOf(a / b);
-	    }
+	public static String tinh(int a, int b) {
+		if (b == 0)
+			return "Không chia được cho 0";
+		return String.valueOf(a / b);
+	}
 }
